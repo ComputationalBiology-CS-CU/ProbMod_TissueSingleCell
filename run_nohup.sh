4dc088663b6289fc884e739ffac34c489fda73e5
haoyu9112 <haoyu9112@gmail.com>
2018-04-25 22:29:31 -0400

    update

--- run_nohup.sh
+++ run_nohup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-nohup sh -c "cat srr_list/1yr.txt | xargs sh run_align.sh" > nohup.log &
+
+array=( 1yr 5yr 6yr 21yr 22yr 38yr 44yr 54yr )
+for i in "${array[@]}"
+do
+    echo $i
+    nohup PREFIX=$i sh -c "cat srr_list/$i.txt | xargs sh run_align.sh" > nohup.log &
+done
+
 
 
